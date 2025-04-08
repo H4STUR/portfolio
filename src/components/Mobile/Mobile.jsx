bb1ed091b34fc9a49ec12140fcb2ed14dae74eb9
H4STUR <lukasz.majerski2000@gmail.com>
2025-04-08 22:00:34 +0200

    small mobile view fix

--- src/components/Mobile/Mobile.jsx
+++ src/components/Mobile/Mobile.jsx
@@ -8,6 +8,18 @@ const Mobile = ({ onBypass }) => {
   const [date, setDate] = useState('');
 
   useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+  
+    setVh();
+    window.addEventListener('resize', setVh);
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+  
+
+  useEffect(() => {
     const updateClock = () => {
       const now = new Date();
       const h = now.getHours().toString().padStart(2, '0');
